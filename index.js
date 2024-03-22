f4f64d085be9407c89ad1b0418d275085bb86fe4
JackyPollich <tuzeopmrkc@outlook.com>
2024-03-23 03:45:42 +0800

    et

---
+++ index.js
@@ -0,0 +1,23 @@
+const sass = require('node-sass')
+
+module.exports = (css, settings) => {
+  const cssWithPlaceholders = css
+    .replace(/\:\s*%%styled-jsx-placeholder-(\d+)%%/g, (_, id) =>
+      `: styled-jsx-placeholder-${id}()`
+    )
+    .replace(/%%styled-jsx-placeholder-(\d+)%%/g, (_, id) =>
+      `/*%%styled-jsx-placeholder-${id}%%*/`
+    )
+
+  const preprocessed = sass.renderSync(Object.assign({
+    data: cssWithPlaceholders
+  }, settings.sassOptions)).css.toString()
+
+  return preprocessed
+    .replace(/\:\s*styled-jsx-placeholder-(\d+)\(\)/g, (_, id) =>
+      `: %%styled-jsx-placeholder-${id}%%`
+    )
+    .replace(/\/\*%%styled-jsx-placeholder-(\d+)%%\*\//g, (_, id) =>
+      `%%styled-jsx-placeholder-${id}%%`
+    )
+}
---
+++ test.js
@@ -0,0 +1,61 @@
+const assert = require('assert')
+const stripIndent = require('strip-indent')
+const plugin = require('./')
+
+const cleanup = str => stripIndent(str).trim()
+
+describe('styled-jsx-plugin-sass', () => {
+
+  it('applies plugins', () => {
+    assert.equal(
+      plugin('p { img { display: block} color: color(red a(90%)) }', {}).trim(),
+      cleanup(`
+        p {
+          color: color(red a(90%)); }
+          p img {
+            display: block; }
+      `)
+    )
+  })
+
+  it('works with placeholders', () => {
+    assert.equal(
+      plugin('p { img { display: block } color: %%styled-jsx-placeholder-0%%; } %%styled-jsx-placeholder-1%%', {}).trim(),
+      cleanup(`
+        p {
+          color: %%styled-jsx-placeholder-0%%; }
+          p img {
+            display: block; }
+
+        %%styled-jsx-placeholder-1%%
+      `)
+    )
+  })
+
+  it('works with @import', () => {
+    assert.equal(
+      plugin('@import "fixture"; p { color: red }', {}).trim(),
+      cleanup(`
+        div {
+          color: red; }
+
+        p {
+          color: red; }
+      `)
+    )
+  })
+
+  it('applies sassOptions', () => {
+    assert.equal(
+      plugin('div { padding: (1 / 3) * 1em }', {
+        sassOptions: {
+          precision: 1
+        }
+      }).trim(),
+      cleanup(`
+        div {
+          padding: 0.3em; }
+      `)
+    )
+  });
+})
